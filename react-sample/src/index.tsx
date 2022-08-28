ac3dd5641a46802568157ea0074351c9e7950101
morihaya <yukiya.hayashi@gmail.com>
2022-08-28 15:23:41 +0900

    feat: Add input box to name

--- react-sample/src/index.tsx
+++ react-sample/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 //import App from './App';
-import Hello from './components/Hello';
+import Name from './components/Name';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <Hello />
+    <Name />
   </React.StrictMode>
 );
 
