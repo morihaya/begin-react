75caec087910813d4179ce5f66bd6572053c5e20
morihaya <yukiya.hayashi@gmail.com>
2022-09-04 23:11:44 +0900

    feat: ContextSample

--- react-sample/src/index.tsx
+++ react-sample/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 //import App from './App';
-import ContainerSample from './components/ContainerSample';
+import ContextSample from './components/ContextSample';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <ContainerSample />
+    <ContextSample />
   </React.StrictMode>
 );
 
